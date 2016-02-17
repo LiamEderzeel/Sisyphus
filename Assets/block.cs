d88ee23a05bf2811daaa89014915374a53653b00
LiamEderzeel <liamederzeel@gmail.com>
2016-02-17 11:18:55 +0100

    Blockjes

---
+++ Assets/block.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class block : MonoBehaviour {
+
+    private Rigidbody _rigidbody;
+
+	void Start ()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
+        ActivatePhysics();
+	}
+
+	void Update ()
+    {
+
+	}
+
+    void DeactivatePhysics()
+    {
+        _rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionX;
+    }
+
+    void ActivatePhysics()
+    {
+        _rigidbody.constraints = RigidbodyConstraints.None;
+    }
+}
+

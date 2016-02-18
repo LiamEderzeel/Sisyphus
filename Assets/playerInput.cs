05bca5207a6dfaf26cf432ad5f497427423a7a21
LiamEderzeel <liamederzeel@gmail.com>
2016-02-18 11:20:50 +0100

    jaajaj

--- Assets/playerInput.cs
+++ Assets/playerInput.cs
@@ -11,6 +11,7 @@ public class playerInput : MonoBehaviour {
     private float screenWidth;
     private float screenHeight;
     private State _state;
+    private block _block;
 
 	void Start ()
     {
@@ -20,40 +21,52 @@ public class playerInput : MonoBehaviour {
         screenHeight = 2f * cam.orthographicSize;
         screenWidth = screenHeight * cam.aspect;
         _stackScript = stack.GetComponent<stack>();
+            letter.SetActive(false);
     }
 
     void Update ()
     {
         if(_state == State.main)
         {
-            letter.SetActive(false);
             if ( Input.GetMouseButtonDown (0)){
                 RaycastHit hit;
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 if ( Physics.Raycast (ray,out hit,100.0f)) {
                     Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
-                    block _block = hit.transform.gameObject.GetComponent<block>();
-                    _block.ActivatePhysics();
-                    _block.Nocolider();
-                    hit.transform.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0,0,1f) * _force);
-                    letter.SetActive(true);
+                    _block = hit.transform.gameObject.GetComponent<block>();
                     ChangeState(State.letter);
                 }
             }
-
-            if(_state == State.letter)
-            {
-                    letter.SetActive(true);
-            }
-
-            if(_state == State.sending)
-            {
-            }
         }
+
+        if(_state == State.letter)
+        {
+        }
+
+    }
+
+    private void Sending()
+    {
+        ChangeState(State.letter);
+        letter.SetActive(false);
+        _block.ActivatePhysics();
+        _block.Nocolider();
+        _block.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0,0,1f) * _force);
+        ChangeState(State.main);
     }
 
     private void ChangeState(State newState)
     {
+        letter.SetActive(false);
+        if(newState == State.letter)
+        {
+            letter.SetActive(true);
+        }
         _state = newState;
+    }
+
+    public void ToSending()
+    {
+        Sending();
     }
 }
